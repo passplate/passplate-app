f9ab7b3222234463c27b1d9c38fe71ae30288589
Summer <spe358@utexas.edu>
2023-10-12 10:15:44 -0500

    recipes showing up in tableview

--- PassplateApp/PassplateApp/ViewController.swift
+++ PassplateApp/PassplateApp/ViewController.swift
@@ -7,28 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     var recipes: Recipes
+    let textCellIdentifier = "TextCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Call your API here
-        // fetchUserData()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     required init?(coder aDecoder: NSCoder) {
-        self.recipes = Recipes(count: nil, next: nil, previous: nil, meals: [])
+        self.recipes = Recipes(count: 0, next: nil, previous: nil, meals: [])
         super.init(coder: aDecoder)
     }
 
        
     @IBAction func buttonPressed(_ sender: UIButton) {
         fetchSearchResults(searchVal: textField.text!)
+//        tableView.reloadData()
     }
 
-    
     func fetchSearchResults(searchVal: String) {
         let url = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + searchVal)
 
@@ -49,7 +51,10 @@ class ViewController: UIViewController {
                     
                     // Decode into list of Recipes
                     self.recipes = try decoder.decode(Recipes.self, from: data)
-                    self.recipes.meals.forEach { recipe in print(recipe) }
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                    self.recipes.meals.forEach { recipe in print(recipe.strMeal) }
                 } catch {
                     print("Failed to load: \(error)")
                 }
@@ -59,6 +64,21 @@ class ViewController: UIViewController {
         }
         
         task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.meals.count
+    }
+    
+    // Displays pizza orders in table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath as IndexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = recipes.meals[row].strMeal
+        cell.textLabel?.numberOfLines = 0
+        
+        return cell
     }
     
   }
