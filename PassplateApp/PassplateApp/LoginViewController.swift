6ac5c84f168584f0e99fd3b6fe75c0cd64cd0d9d
Anne-Marie Prosper <anniethepro@gmail.com>
2023-10-16 16:28:29 -0500

    finished storyboard and segues for signup and signin screens

--- PassplateApp/PassplateApp/LoginViewController.swift
+++ PassplateApp/PassplateApp/LoginViewController.swift
@@ -9,23 +9,51 @@ import UIKit
 import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
-    // SigninSegue
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    //CreateAccountSegue
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        passwordTextField.isSecureTextEntry = true
+        
+        Auth.auth().addStateDidChangeListener() {
+            (auth, user) in
+            if user != nil {
+                self.performSegue(withIdentifier: "SigninSegue", sender: self)
+                self.emailTextField.text = nil
+                self.passwordTextField.text = nil
+            }
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    // ERROR MESSAGES:
+    // email not found (?) 
+    // password incorrect
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    // Complete for Beta release
+    // Segues to a new screen where user can input their email, and it will prompt them to update their password
+    @IBAction func forgotPassword(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func signinButtonPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+            (authResult,error) in
+            //            if let error = error as NSError? {
+            //                self.errorMessage.text + "\(error.localizedDescription)"
+            //            } else {
+            // self.errorMessage.text = ""
+            //}
+        }
+        
+    }
 }
